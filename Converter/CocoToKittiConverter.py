159c4f373aa444a2b3f22b1f7d73ebfdf0c982b7
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2022-08-09 10:46:07 +0530

    resize image integrated with cocotoyoloconverter

--- Converter/CocoToKittiConverter.py
+++ Converter/CocoToKittiConverter.py
@@ -1,18 +1,48 @@
 import pycocotools.coco as coco
 import os
 import sys
+import cv2
 
 class CocoToKittiConverter:
-    def __init__(self, path, kittiPath = '/CocoKittiDataset/') -> None:
+    def __init__(self, jsonPath, imagePath , kittiPath = '/CocoKittiDataset/', height = None, width = None ) -> None:
+        '''
+            initializes the converter
+            path = 'path to the coco dataset json'
+            imagePath = 'path to the coco dataset images\' directory'
+            kititPath = 'path where resultant datset is stored'
+            height = 'resized height of the image'
+            width = 'resized width of the image'
+        '''
         self.idCriteriaDict = dict()
-        self.path = path
-        self.dataset = coco.COCO(path)
+        self.jsonPath = jsonPath
+        self.imagePath = imagePath
+        self.dataset = coco.COCO(jsonPath)
         self.kittiPath = sys.path[0] + kittiPath
+        self.annotationsPath = self.kittiPath + '\\annotations\\'
+        self.kittiImgPath = self.kittiPath + '\\images\\'
+        
+        self.height, self.width = None, None
+        
+        if height is not None and width is not None:
+            self.height = height
+            self.width = width
+        
         # self.imgToBBoxDict = dict()
-        print('Making up the yolo dataset directory at location ' + self.kittiPath)
 
+        print('kitti dataset directory location ' + self.kittiPath)        
         if not os.path.exists(self.kittiPath):
+            print('Making up the kitti dataset directory at location ' + self.kittiPath)
             os.makedirs(self.kittiPath)
+        
+            
+        if not os.path.exists(self.annotationsPath):
+            print('Making up the kitti dataset annotation directory at location ' + self.annotationsPath)
+            os.makedirs(self.annotationsPath)
+            
+                    
+        if not os.path.exists(self.kittiImgPath):
+            print('Making up the kitti dataset images directory at location ' + self.kittiImgPath)
+            os.makedirs(self.kittiImgPath)
                 
         for i, cat in enumerate(self.dataset.dataset['categories']):
             self.idCriteriaDict[cat['id']] = cat['name']
@@ -29,24 +59,40 @@ class CocoToKittiConverter:
         for imgid in imgids:
             imageToAnns[imgid] = self.dataset.loadAnns(self.dataset.getAnnIds(imgid))
             
+        # in case height and width is not provided
+        x_scaled = 1.0
+        y_scaled = 1.0
+        
         for imgid  in imgids:
             fileinput = ''
             img = self.dataset.loadImgs(imgid)[0]
+            imgHeight = img['height']
+            imgWidth = img['width']
+            
+            image = cv2.imread(os.path.join(self.imagePath, img['file_name']))
+
+            if self.height is not None and self.width is not None:
+                image = cv2.resize(image, (self.width, self.height))
+                x_scaled = self.width /  imgWidth 
+                y_scaled = self.height / imgHeight 
+            
+            cv2.imwrite(os.path.join(self.kittiImgPath, img['file_name']), image)                
+            
             for ann in imageToAnns[imgid]:
                 fileinput += self.idCriteriaDict[ann['category_id']] + ' '
                 fileinput += '_ _ _ '
-                fileinput += ' '.join([str(elem) for elem in self.convertToKitti(ann['bbox'])]) + ' '
+                fileinput += ' '.join([str(elem) for elem in self.convertToKitti(ann['bbox'], x_scaled, y_scaled)]) + ' '
                 fileinput +=  '_ _ _ _ _ _ _\n'
         
-            with open(os.path.join(self.kittiPath, str(img['file_name'][:-4]) + '.txt'), 'w') as f:
+            with open(os.path.join(self.annotationsPath, str(img['file_name'][:-4]) + '.txt'), 'w') as f:
                 f.write(fileinput)
 
-    def convertToKitti(self, bbox):
+    def convertToKitti(self, bbox, x_scaled, y_scaled):
         '''
             converts the coco bbox to kitti bbox
         '''
-        xmin = float(bbox[0])
-        ymin = float(bbox[1])
-        xmax = float(bbox[2])
-        ymax = float(bbox[3])
+        xmin = float(bbox[0] * x_scaled)
+        ymin = float(bbox[1] * y_scaled)
+        xmax = float(bbox[2] * x_scaled)
+        ymax = float(bbox[3] * y_scaled)
         return [xmin, ymin, xmax + xmin, ymax + ymin]
--- Converter/CocoToYoloConverter.py
+++ Converter/CocoToYoloConverter.py
@@ -1,21 +1,37 @@
 import pycocotools.coco as coco
 import os
 import sys
+import cv2
 
 class CocoToYoloConverter:
-    def __init__(self, path, yoloPath = '/CocoYoloDataset/') -> None:
+    def __init__(self, jsonpath, imagePath, yoloPath = '/CocoYoloDataset/', height = None, width = None) -> None:
+        '''
+            initializes the converter
+            jsonPath = 'path to the coco dataset json'
+            imagePath = 'path to the coco dataset images\' directory'
+            yoloPath = 'path where resultant datset is stored'
+            height = 'resized height of the image'
+            width = 'resized width of the image'
+        '''
         self.criteriaIndexDict = dict()
-        self.path = path
-        self.dataset = coco.COCO(path)
+        self.jsonpath = jsonpath
+        self.dataset = coco.COCO(jsonpath)
         self.yoloPath = sys.path[0] + yoloPath
+        self.imagePath = imagePath
         # self.imgToBBoxDict = dict()
         print('Making up the yolo dataset directory at location ' + self.yoloPath)
+
+        self.height = height if height is not None else None
+        self.width = width if width is not None else None
 
         if not os.path.exists(self.yoloPath):
             os.makedirs(self.yoloPath)
         
         if not os.path.exists(self.yoloPath + 'annotations/'):
             os.makedirs(self.yoloPath + 'annotations/')
+            
+        if not os.path.exists(self.yoloPath + 'images/'):
+            os.makedirs(self.yoloPath + 'images/')
         
         self.createLabelFile()
         self.convert()
@@ -39,6 +55,7 @@ class CocoToYoloConverter:
         '''
         imageToAnns = dict()
         imgids = self.dataset.getImgIds()
+        x_scale, y_scale = 1, 1
         
         for imgid in imgids:
             imageToAnns[imgid] = self.dataset.loadAnns(self.dataset.getAnnIds(imgid))
@@ -46,8 +63,33 @@ class CocoToYoloConverter:
         for imgid  in imgids:
             fileinput = ''
             img = self.dataset.loadImgs(imgid)[0]
+            
+            image = cv2.imread(os.path.join(self.imagePath, img['file_name']))
+            
+            if self.height is not None and self.width is not None:
+                x_scale = self.width / img['width']
+                y_scale = self.height / img['height']
+                image = cv2.resize(image, (self.width, self.height))
+                
+            cv2.imwrite(os.path.join(self.yoloPath + 'images/', img['file_name']), image)
+
+            
             for ann in imageToAnns[imgid]:
-                fileinput += str(self.criteriaIndexDict[self.dataset.loadCats(ann['category_id'])[0]['name']]) + ' '+' '.join([str(elem) for elem in ann['bbox']]) + '\n'
+                fileinput += str(self.criteriaIndexDict[self.dataset.loadCats(ann['category_id'])[0]['name']]) + ' ' + self.convertBBox(ann['bbox'], x_scale, y_scale) + '\n'
         
             with open(os.path.join(self.yoloPath + 'annotations/', str(img['file_name'][:-4]) + '.txt'), 'w') as f:
                 f.write(fileinput)
+                
+    def convertBBox(self, bbox , x_scale, y_scale):
+        '''
+            scale the bboxes
+        '''
+        bbox[0] = bbox[0] * x_scale
+        bbox[1] = bbox[1] * y_scale
+        bbox[2] = bbox[2] * x_scale
+        bbox[3] = bbox[3] * y_scale
+            
+        return ' '.join([str(elem) for elem in bbox])
+        
+        
+        
